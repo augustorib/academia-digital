af8bb1d048cec46a71ded4f1d8ef5904defa9ec6
augustorib <augustorifra@gmail.com>
2024-04-11 02:16:01 -0300

    feat: Validation and BD yml config

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -3,7 +3,13 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -11,11 +17,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AlunoForm {
 
+  @NotBlank(message = "Preencha o nome do aluno")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String nome;
 
+  @NotBlank(message = "Preencha o nome do aluno")
+  @CPF(message = "'${validatedValue}' é invalido!")
   private String cpf;
 
+  @NotBlank(message = "Preencha o nome do aluno")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
   private String bairro;
 
+  @NotNull(message = "Preencha o campo corretamente")
+  @Past(message = "'${validatedValue}' é invalida.")
   private LocalDate dataDeNascimento;
 }
